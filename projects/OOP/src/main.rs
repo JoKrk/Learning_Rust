efb01b485210696e8150143bb42353133b8eee98
Joseph Kirk <Joseph.Kirk@m-solv.com>
2021-09-24 16:36:51 +0100

    finished working through the OOP chapter

--- projects/OOP/src/main.rs
+++ projects/OOP/src/main.rs
@@ -1,5 +1,18 @@
+use OOP::Post;
+
 fn main() {
-    println!("Hello, world!");
+
+
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad today for lunch");
+
+    let post = post.request_review();
+    
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+
 }
 
 
